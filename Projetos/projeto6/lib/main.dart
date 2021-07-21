8b5824e38cc2748f109a8a8a9d60fc4e93940a1e
Gustavo Corrêa Mendes <67390202+gustavomendesof@users.noreply.github.com>
2021-07-20 21:57:25 -0300

    Atualizado

--- Projetos/projeto6/lib/main.dart
+++ Projetos/projeto6/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'guides.dart';
 import 'drawer.dart';
+import 'snackbar.dart';
+import 'orientation.dart';
+import 'fonts.dart';
+import 'theme.dart';
 
 void main() {
-  runApp(MaterialApp(
-    //home: ComLayout(),
-    home: ComDraw(),
-  ));
+  runApp(
+    MaterialApp(
+      //home: ComLayout(),
+      //home: ComDraw(),
+      //home: ComSnack(),
+      //home: ComOrientation(),
+      //home: ComFonts(),
+      home: ComThemes(),
+    ),
+  );
 }
